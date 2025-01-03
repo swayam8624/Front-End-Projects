8a93d975466011970c295c426b54d112e5e2fd15
swayam singal <swayam.singal@gmail.com>
2025-01-03 12:04:57 +0530

    animations

--- Portfolio-1/script.js
+++ Portfolio-1/script.js
@@ -3,33 +3,59 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+function firstPageAnim() {
+  var tl = gsap.timeline();
 
-// function circleflattening() {
-//   // define default scale value
-//   var xscale = 1;
-//   var yscale = 1;
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundingelem", {
+      y: 0,
+      ease: Expo.easeInOut,
+      duration: 2,
+      delay: -1,
+      stagger: 0.2,
+    })
+    .from("#herofooter", {
+      y: -10,
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
+}
 
-//   var xprev = 0;
-//   var yprev = 0;
+let timeout;
 
-//   window.addEventListener("mousemove", function (dets) {
-//     clearTimeout(timeout);
+function circleflattening() {
+  // define default scale value
+  var xscale = 1;
+  var yscale = 1;
 
-//     xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
-//     yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+  var xprev = 0;
+  var yprev = 0;
 
-//     xprev = dets.clientX;
-//     yprev = dets.clientY;
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
 
-//     circleMouseFollower(xscale, yscale);
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
 
-//     timeout = setTimeout(function () {
-//       document.querySelector(
-//         "#minicircle"
-//       ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
-//     }, 100);
-//   });
-// }
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circleMouseFollower(xscale, yscale);
+
+    timeout = setTimeout(function () {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+    }, 100);
+  });
+}
 
 
 function circleMouseFollower(xscale =1 , yscale =1 ) {
@@ -42,5 +68,32 @@ function circleMouseFollower(xscale =1 , yscale =1 ) {
   });
 }
 
-// circleflattening();
+circleflattening();
 circleMouseFollower();
+firstPageAnim();
+
+document.querySelectorAll(".elem").forEach(function (elem) {
+  let rotate = 0;
+  let diffrot = 0;
+
+  elem.addEventListener("mouseleave", function (dets) {
+    gsap.to(elem.querySelector("img"), {
+      opacity: 0,
+      ease: Power3,
+      duration: 0.5,
+    });
+  });
+
+  elem.addEventListener("mousemove", function (dets) {
+    let diff = dets.clientY - elem.getBoundingClientRect().top ;
+    diffrot = dets.clientX - rotate ;
+    rotate = dets.clientX;
+    gsap.to(elem.querySelector("img"), {
+      opacity: 1,
+      ease: Power3,
+      top: diff-100 ,
+      left: dets.clientX -100,
+      rotate: gsap.utils.clamp(-20, 20, diffrot * 0.5),
+    });
+  });
+});
